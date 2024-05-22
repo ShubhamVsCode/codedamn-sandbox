d23202d7df50256d0d58931658f269e0a3e0c8f0
shubham <shubhamvscode@gmail.com>
2024-05-22 18:57:18 +0530

    feat: running app port bug fixed

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.get("/health", (req, res) => {
   console.log(req.headers);
@@ -41,19 +41,18 @@ app.get("/health", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  const runningAppPort = req.cookies.port;
-  console.log(runningAppPort, "PORT");
-  // console.log(`Running app port: ${runningAppPort}`);
-  // if (runningAppPort) {
-  //   const target = `http://localhost:${runningAppPort}`;
-  //   console.log(`Proxying request to ${target}`);
-  //   return createProxyMiddleware({
-  //     target,
-  //     changeOrigin: true,
-  //   })(req, res, next);
-  // } else {
-  next();
-  // }
+  const runningAppPort = req.headers["x-forwarded-port"];
+  console.log(`Running app port: ${runningAppPort}`);
+  if (runningAppPort) {
+    const target = `http://localhost:${runningAppPort}`;
+    console.log(`Proxying request to ${target}`);
+    return createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    })(req, res, next);
+  } else {
+    next();
+  }
 });
 io.on("connection", (socket) => {
   console.log("Connecting to socket");
